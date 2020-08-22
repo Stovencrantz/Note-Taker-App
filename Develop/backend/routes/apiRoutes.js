29d54a733ca8149b21279bd868e9b2dd8854b935
Stovencrantz <sknapp143@gmail.com>
2020-08-21 23:20:23 -0400

    created working route for our api

---
+++ Develop/backend/routes/apiRoutes.js
@@ -0,0 +1,11 @@
+//brings in the data stored in our db.json file
+var dbData = require("../db.json");
+
+//exports all of the content within our anonymous function
+module.exports = function(app) {
+
+    //displays the note titles and notes contained in our db.json file as JSON data
+    app.get("/api/db", function(req, res){
+        res.json(dbData);
+    })
+}
